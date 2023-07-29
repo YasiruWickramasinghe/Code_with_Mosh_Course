bdc9f2e6dd8058a943e32613ea3f9c1d920bcfb9
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-07-29 11:59:43 +0530

    Inilialize new course - Typescript

---
+++ TypeScript/my-code/src/index.ts
@@ -0,0 +1,67 @@
+// 1 - getting start
+
+    // let age: number =20;
+    // if (age < 50)
+    //     age += 10;
+
+    //     console.log(age)
+
+// 2 - fundamental
+
+        // let sales = 123_456_789;
+        // let course = "TypeScriptt";
+        // let is_published = true;
+        // let level;
+        // level = 1
+        // level = "a"
+
+        // function render(document: any){
+        //     console.log(document);
+        // }
+
+    // array
+
+        // let number1 = [1, 2, '3']
+        // let number2: number[] = [1, 2, '3']
+        // let number3 = []
+
+    //tuple - fixed length
+
+        // let user: [number, string] = [1, 'mosh']
+
+    // enum
+
+        // const enum Size {Small = 1, Medium, Large};
+        // let mySize: Size = Size.Medium
+        // console.log(mySize);
+
+    // functions
+
+            // function calculateTax(income: number, taxYear = 2022): number {
+            //     // return 'a'
+            //     // return 0;
+
+            //     // let x;
+
+            //     if (taxYear < 2022)
+            //         return income * 1.2;
+            //     return income * 1.3;  
+            // }
+
+            // calculateTax(10000, 2023)
+            // calculateTax(30000)
+
+    // object
+
+            // let employee: {
+            //     readonly id: number,
+            //     name: string,
+            //     retire: (date: Date) => void
+            // } = {
+            //     id: 1,
+            //     name: 'Mosh',
+            //     retire: (date: Date) => {
+            //         console.log(date)
+            //     }
+            // }
+
